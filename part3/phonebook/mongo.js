53e91e1c31aac2f6a5ce8c413b03a9afc2712f98
poliveir <pedrol2507@gmail.com>
2025-08-12 14:22:42 +0100

    3.12: Command-line database

---
+++ part3/phonebook/mongo.js
@@ -0,0 +1,50 @@
+const mongoose = require('mongoose');
+
+const argvLen = process.argv.length;
+if (argvLen < 4) {
+	console.error('Please provide the username and password as an argument: node mongo.js <username> <password>');
+	process.exit(1);
+
+}
+
+const user = process.argv[2];
+const password = process.argv[3];
+const url = `mongodb+srv://${user}:${password}@cluster-fullstackopen.ce7xo32.mongodb.net/?retryWrites=true&w=majority&appName=Cluster-fullstackopen`
+
+mongoose.set('strictQuery', false);
+mongoose.connect(url);
+
+const contactSchema = new mongoose.Schema(
+	{
+		name: String,
+		number: String
+	}
+)
+
+const Contact = mongoose.model('Contact', contactSchema);
+
+if (argvLen == 4) {
+	console.log('phonebook:');
+	Contact
+		.find({})
+		.then(contacts => {
+			contacts.forEach(c => console.log(c.name, c.number));
+			mongoose.connection.close();
+		});
+}
+else if (argvLen == 6) {
+	const contact = new Contact({
+		name: process.argv[4],
+		number: process.argv[5]
+	});
+	contact
+		.save()
+		.then(() => {
+			console.log(`added ${contact.name} number ${contact.number} to phonebook`);
+			mongoose.connection.close();
+		});
+}
+else {
+	console.error('Please provide the name and number as an argument: node mongo.js <username> <password> <name> <number>');
+	process.exit(1);
+}
