818244f9c70b68ab9aa0a14336b589906f455414
poliveir <pedrol2507@gmail.com>
2025-08-13 13:57:34 +0100

    3.22: Lint configuration

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,71 +1,71 @@
-const express = require('express');
-var morgan = require('morgan');
-const app = express();
-require('dotenv').config();
-const Contact = require('./models/contact');
+const express = require('express')
+var morgan = require('morgan')
+const app = express()
+require('dotenv').config()
+const Contact = require('./models/contact')
 
 app.use(express.static('dist'))
-app.use(express.json());
+app.use(express.json())
 morgan.token(
 	'body',
-	function (req, res) {
-		return JSON.stringify(req.body);
+	function (req) {
+		return JSON.stringify(req.body)
 	}
 )
-app.use(morgan(':method :url :status :response-time ms - :res[content-length] :body'));
+app.use(morgan(':method :url :status :response-time ms - :res[content-length] :body'))
 
-app.get("/api/contacts", (req, res, next) => {
+app.get('/api/contacts', (req, res, next) => {
 	Contact
 		.find({})
 		.then(contacts => res.json(contacts))
-		.catch(error => next(error));
-});
+		.catch(error => next(error))
+})
 
 app.get('/api/contacts/:id', (req, res, next) => {
 	Contact
 		.findById(req.params.id)
 		.then(contact => {
 			if (contact)
-				res.json(contact);
+				res.json(contact)
 			else
-				res.status(404).end();
+				res.status(404).end()
 		})
-		.catch(error => next(error));
-});
+		.catch(error => next(error))
+})
 
 app.delete('/api/contacts/:id', (req, res, next) => {
 	Contact
 		.findByIdAndDelete(req.params.id)
 		.then(() => res.status(204).end())
-		.catch(error => next(error));
-});
+		.catch(error => next(error))
+})
 
 app.post('/api/contacts', (req, res, next) => {
-	const contact = req.body;
+	const contact = req.body
 
 	Contact
-		.find({name: contact.name})
+		.find({ name: contact.name })
 		.then(existingContact => {
 			if (existingContact.length > 0)
 				return res.status(400).json({
-					error:  "name must be unique"
-				});
+					error:  'name must be unique'
+				})
 			else {
 				const newContact = new Contact({
 					name: contact.name,
 					number: contact.number
-				});
+				})
 				newContact
 					.save()
 					.then(savedContact => res.json(savedContact))
-					.catch(error => next(error));
+					.catch(error => next(error))
 			}
 		})
-		.catch(error => next(error));
-});
+		.catch(error => next(error))
+})
 
 app.put('/api/contacts/:id', (req, res, next) => {
-	const contact = req.body;
+	const contact = req.body
 	Contact
 		.findByIdAndUpdate(
 			req.params.id,
@@ -76,32 +76,32 @@ app.put('/api/contacts/:id', (req, res, next) => {
 			}
 		)
 		.then(updatedContact => res.json(updatedContact))
-		.catch(error => next(error));
-});
+		.catch(error => next(error))
+})
 
-app.get("/info", (req, res, next) => {
+app.get('/info', (req, res, next) => {
 	Contact
 		.countDocuments()
 		.then(count => res.send(
 			`<p>Phonebook has info for ${count} people</p>\
 			<p>${new Date()}</p>`
 		))
-		.catch(error => next(error));
-});
+		.catch(error => next(error))
+})
 
-const errorHandler = (err, req, res, next) => {
-	console.error(err.message);
+const errorHandler = (err, req, res) => {
+	console.error(err.message)
 
 	if (err.name === 'CastError')
-		return res.status(400).send({error: "malformatted id"});
+		return res.status(400).send({ error: 'malformatted id}' })
 	else if (err.name === 'ValidationError')
-    	return res.status(400).json({ error: err.message })
+		return res.status(400).json({ error: err.message })
 	else
-		return res.status(500).send({error: err.message});
-};
-app.use(errorHandler);
+		return res.status(500).send({ error: err.message })
+}
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
-	console.log(`Server running on port ${PORT}`);
-});
+	console.log(`Server running on port ${PORT}`)
+})
--- part3/phonebook/models/contact.js
+++ part3/phonebook/models/contact.js
@@ -1,17 +1,17 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
 
-mongoose.set('strictQuery', false);
+mongoose.set('strictQuery', false)
 
-const url = process.env.MONGODB_URI;
+const url = process.env.MONGODB_URI
 
 mongoose
 	.connect(url)
 	.then(() => {
-		console.log('connected to MongoDB');
+		console.log('connected to MongoDB')
 	})
 	.catch((error) => {
-		console.log('error connecting to MongoDB:', error.message);
-	});
+		console.log('error connecting to MongoDB:', error.message)
+	})
 
 const contactSchema = new mongoose.Schema({
 	name: {
@@ -25,14 +25,14 @@ const contactSchema = new mongoose.Schema({
 		required: true,
 		validate: [(val) =>  /^\d{2,3}-\d+$/.test(val), '{VALUE} is not a valid phone number. Phone number should be in the format 00-0000000 or 000-0000000']
 	}
-});
+})
 
 contactSchema.set('toJSON', {
 	transform: (document, returnedObject) => {
-		returnedObject.id = returnedObject._id.toString();
-		delete returnedObject._id;
-		delete returnedObject.__v;
+		returnedObject.id = returnedObject._id.toString()
+		delete returnedObject._id
+		delete returnedObject.__v
 	},
-});
+})
 
-module.exports = mongoose.model('Contact', contactSchema);
+module.exports = mongoose.model('Contact', contactSchema)
